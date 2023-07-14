d45be75cc8c57161e166e8a7ac844f41f374ac2d
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-07-14 16:18:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # Required Imports
 import streamlit as st
 import openai
@@ -8,7 +7,7 @@ openai.api_key = st.secrets["openai_api_key"]
 
 # Story Settings
 story_settings = {
-    '''Tief inmitten der grünen Wiesen, versteckt unter dem schützenden Schatten eines uralten Baumstumpfes, 
+    "The Adventures of Family Mouse": '''Tief inmitten der grünen Wiesen, versteckt unter dem schützenden Schatten eines uralten Baumstumpfes, 
     liegt das gemütliche Heim der liebenswerten Mäusefamilie Körnchen. Mama und Papa Körnchen teilen ihr Zuhause 
     mit ihren drei entzückenden Kindern: der neugierigen Emma, dem abenteuerlustigen Max und dem jüngsten, 
     dem sanften kleinen Tim. Tim hat eine besondere Freundin namens Greta, eine Grille mit bemerkenswerten 
@@ -28,14 +27,30 @@ story_settings = {
     wunderschönen Natur und lebhaften Dialogen zwischen den Tieren, die oft humorvoll sind und die Persönlichkeiten 
     der Figuren widerspiegeln. Aktiv erzählt, fühlen sich die jungen Zuhörer direkt in die Geschichte hineinversetzt. 
     So sind die Abenteuer der Mäusefamilie Körnchen ein friedvoller Abschluss eines jeden Tages, voller kleiner 
-    Weisheiten und lehrreicher Momente, die die Kinder sanft in den Schlaf begleiten.''' : "Eine Mäusegeschichte",
-    "Ferdinand, the red racing Car": "Ferdinand, das rote Auto"
+    Weisheiten und lehrreicher Momente, die die Kinder sanft in den Schlaf begleiten.''',
+    "Ferdinand, the red racing Car": "In a bustling city, a shiny red racing car named Ferdinand resides. Ferdinand is known for his speed and agility, bringing joy to everyone who sees him race."
+}
+
+story_options = {
+    "deutsch": ["Eine Mäusegeschichte", "Ferdinand, das rote Auto"],
+    "english": ["The Adventures of Family Mouse", "Ferdinand, the red racing Car"],
+    "espanol": ["Las Aventuras de la Familia Ratón", "Ferdinand, el coche rojo"]
+}
+
+# This dictionary maps the displayed names to the actual keys in `story_settings`.
+story_mapping = {
+    "Eine Mäusegeschichte": "The Adventures of Family Mouse",
+    "Ferdinand, das rote Auto": "Ferdinand, the red racing Car",
+    "The Adventures of Family Mouse": "The Adventures of Family Mouse",
+    "Ferdinand, the red racing Car": "Ferdinand, the red racing Car",
+    "Las Aventuras de la Familia Ratón": "The Adventures of Family Mouse",
+    "Ferdinand, el coche rojo": "Ferdinand, the red racing Car"
 }
 
 def get_story_titles(prompt):
     # Generate story titles with GPT-4
     response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=500)
-    titles = response.choices[0].text.strip().split('\n')
+    titles = response.choices[0].text.strip().split('\\n')
     return titles
 
 def get_story(prompt):
@@ -59,11 +74,11 @@ def main():
     st.subheader("Choose your story")
 
     # Dropdown for Story Selection
-    story = st.selectbox("Select a Story", list(story_settings.values()) if language == 'deutsch' else list(story_settings.keys()))
+    story = st.selectbox("Select a Story", story_options[language])
 
     # Get Story Titles
     if st.button("Get Story Titles"):
-        prompt = f"In the language of {language}, using the setting of '{story_settings[story]}', generate 10 engaging and fun titles for a bedtime story."
+        prompt = f"In the language of {language}, using the setting of '{story_settings[story_mapping[story]]}', generate 10 engaging and fun titles for a bedtime story."
         titles = get_story_titles(prompt)
         st.session_state.titles = titles
 
@@ -73,7 +88,7 @@ def main():
 
         # Generate Story
         if st.button("Write this story now"):
-            prompt = f"In the language of {language}, using the setting of '{story_settings[story]}', and with the title '{st.session_state.title}', generate a captivating and age-appropriate bedtime story. Make sure the story is engaging, fun, and has a clear beginning, middle, and end."
+            prompt = f"In the language of {language}, using the setting of '{story_settings[story_mapping[story]]}', and with the title '{st.session_state.title}', generate a captivating and age-appropriate bedtime story. Make sure the story is engaging, fun, and has a clear beginning, middle, and end."
             st.session_state.story = get_story(prompt)
 
     # Show Story
