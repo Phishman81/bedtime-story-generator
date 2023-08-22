c54532b3dd2d5b55b1e4fd3925357d15630e627b
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-08-23 01:36:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-
-def clean_generated_text(text):
-    replacements = {
-        "{st.session_state.character1_name}": "Charakter1",
-        "{st.session_state.character2_name}": "Charakter2",
-        "{st.session_state.title}": "Titel der Geschichte"
-    }
-    for placeholder, replacement in replacements.items():
-        text = text.replace(placeholder, replacement)
-    return text
-
-
-def create_outline(prompt, title, chosen_story):
-    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=500)
-    outline_text = response.choices[0].text.strip()
-    outline_text = outline_text.replace('{story_settings[chosen_story]}', story_settings[chosen_story])
-    return outline_text
-
-# Required Imports
 import streamlit as st
 import openai
-
-# Load GPT-4 API Key
-openai.api_key = st.secrets["openai_api_key"]
 
 # Story Settings
 story_settings = {
@@ -29,16 +7,22 @@ story_settings = {
     "Ferdinand, das rote Auto": "Ferdinand: Das ist ein leuchtend rotes Rennauto, das in der kleinen Stadt Autoburg lebt. Seine Garage befindet sich am Fuße eines großen Berges, umgeben von kurvigen Straßen. Zu seinen Freunden gehören Paula, das freundliche Polizeiauto, Timo, der fleißige Traktor, und Lina, der fröhliche Lastwagen. Es gibt auch Herausforderer wie Viktor, das eitle Rennauto, und Maxi, das schelmische Motorrad, das ständig für Unruhe sorgt."
 }
 
+def create_outline(prompt, title, chosen_story):
+    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=500)
+    outline_text = response.choices[0].text.strip()
+    outline_text = outline_text.replace('{story_settings[chosen_story]}', story_settings[chosen_story])
+    return outline_text
+
 # Function to generate story titles with GPT-4
 def get_story_titles(prompt):
     response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=500)
-    titles = response.choices[0].text.strip().split('\n')  # Updated here
+    titles = response.choices[0].text.strip().split('\n')
     return titles
 
 # Function to generate story with GPT-4
 def get_story(prompt):
     response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=3500)
-    return clean_generated_text(response.choices[0].text.strip())
+    return response.choices[0].text.strip()
 
 # Function to generate image with DALL-E
 def get_image(prompt):
@@ -74,16 +58,14 @@ def main():
     if 'titles' in st.session_state:
         st.session_state.title = st.selectbox("Wähle einen Titel", st.session_state.titles)
 
-        
     # Create Story Outline
-    outline = ""
     if st.button("Outline erstellen"):
         outline_prompt = f"Erstelle einen Outline für die Geschichte mit dem Titel '{{st.session_state.title}}' basierend auf dem folgenden Setting: {{story_settings[chosen_story]}}"
         outline = create_outline(outline_prompt, st.session_state.title, chosen_story)
         st.session_state.outline = outline
         st.write(outline)
 
-# Generate Story
+        # Generate Story
         if 'outline' in st.session_state and 'title' in st.session_state and st.button("Schreibe jetzt diese Geschichte"):
             prompt = f"Du bist ein erfahrener Kinderbuchautor mit viel Wissen über Dramaturgie und liebevolle Dialoge. Du kennst das Prinzip der Heldenreise von Joseph Campbell. Verfasse bitte nach dem Framework der Heldenreise eine spannende Geschichte für Kinder im Alter von 3 bis 6 Jahren über '{story_settings[chosen_story]}'. Die Geschichte hat den Titel '{st.session_state.title}'. Bitte halte dich nur lose an die Vorgaben, du hast viel Spielraum und kreative Freiheit. Bitte nutze eine klare Struktur, erstelle Zwischenüberschriften und mache die Geschichte spannend, aber altersgerecht. Nutze sehr einfache Sprache und keine Fremdwörter."
             st.session_state.story = get_story(prompt)
@@ -93,11 +75,10 @@ def main():
             image_prompt = f"Captivating illustration showing a playful {subject} dealing with '{st.session_state.title}'."
             st.session_state.image_url = get_image(image_prompt)
 
-
     # Show Image and Story
     if 'image_url' in st.session_state and st.session_state.image_url is not None:
         st.image(st.session_state.image_url)
-    
+
     if 'story' in st.session_state and 'title' in st.session_state:
         st.markdown("# " + st.session_state.title)
         st.markdown("## " + st.session_state.story)
